b487935da6525102ba0155695cc1b9b1ed2fae5e
najmi10 <53500211+najmi10@users.noreply.github.com>
2019-10-03 12:57:28 +0700

    Add files via upload

---
+++ LatihanPBO2.java
@@ -0,0 +1,23 @@
+package latihanpob1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ica
+ */
+public class LatihanPBO2 {
+    public static void main(String[] args){
+        int nilai_pbo, nilai_web;
+        nilai_pbo = 90;
+        nilai_web = 80;
+        System.out.println("Variabel yang terdapat dalam program: ");
+        System.out.println("Nilai PBO = " + nilai_pbo);
+        System.out.println("Nilai WEB = " + nilai_web);
+    }
+    
+}
