0edd5557152abb62f3a9eedf347051f7475d6993
najmi10 <53500211+najmi10@users.noreply.github.com>
2019-10-03 17:40:45 +0700

    Add files via upload

---
+++ LatihanPBO1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo1;
+
+/**
+ *
+ * @author ica
+ */
+public class LatihanPBO1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Selamat Belajar di program Java");
+    }
+    
+}
+   
